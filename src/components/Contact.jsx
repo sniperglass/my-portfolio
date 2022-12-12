7f8ccb292190d0b38a09b8fa0145876c5da0cf94
sniperglass <immanuel.jaeggi@gmail.com>
2022-12-12 11:26:49 +0100

    contact refactor xs devices

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const contact = () => {
     from-black to-gray-600 p-4 text-white'>
         <div className='flex flex-col p-4 justify-center max-w-screen-lg
         mx-auto h-full xs:mt-15'>
-            <div className='pb-4'>
+            <div className='xs:mt-16 pb-4'>
                 <p className='xs:text-3xl text-4xl font-bold inline border-b-4
                 border-gray-500'>
                     Contact
