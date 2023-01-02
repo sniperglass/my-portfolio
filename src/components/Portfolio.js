1125a6ee63114c6520a131717455d39478b9fa11
sniperglass <immanuel.jaeggi@gmail.com>
2023-01-02 13:31:31 +0100

    minor refactor

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -37,10 +37,10 @@ const Portfolio = () => {
   return (
     <div name='portfolio' className='bg-gradient-to-b
     from-black to-gray-800 w-full text-white
-    md:h-screen'>
+    '>
 
       <div className='max-w-screen-lg p-7 mx-auto flex 
-      flex-col justify-center w-full h-auto xs:mt-20 mt-10 pt-20'>
+      flex-col justify-center w-full h-auto xs:mt-20 mt-10 pt-20 md:h-screen'>
        
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4
@@ -54,11 +54,8 @@ const Portfolio = () => {
 
           {
             portfolios.map(({id, src, href, text}) => (
-
-                <div key={id}
-                       
-                >
-                  
+                <div key={id}               
+                >       
                   <div className='flex items-center justify-center
                   px-6 py-3 m-4
                   '>
@@ -72,7 +69,7 @@ const Portfolio = () => {
                         '/>
 
                       <div
-                          className='xs:px-8 px-12 md:px-10 m-4 duration-300 hover:scale-110' 
+                          className='xs:px-8 px-12 xs:m-5 m-8 md:my-4 md:px-10 flex justify-center duration-300 hover:scale-110' 
                       >               
                         {text}    
                       </div>
