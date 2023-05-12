e4bbe15a42287bd140df22ebcb19c1954a2075de
Immanuel <immanuel.jaeggi@gmail.com>
2023-05-12 11:09:11 +0200

    improvement to uiux

--- src/components/UIUX.jsx
+++ src/components/UIUX.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import iphone from '../assets/tryagain.png'
+import iphone from '../assets/mockup.png'
 import { FaLongArrowAltRight } from 'react-icons/fa';
 
 const Uiux = () => {
@@ -9,9 +9,10 @@ const Uiux = () => {
     <div name='UI/UX' className='w-full h-screen bg-orange-400'>
 
         <div className='max-w-screen-lg p-7 md:p-4 mx-auto flex
-        flex-col w-full justify-center md:h-screen pt-28 xs:pt-20'>
+        flex-col w-full justify-center md:h-screen pt-28
+          '>
 
-            <div>           
+            <div className='xs:mt-20'>           
                 <p className='text-3xl md:text-4xl inline font-bold text-white border-b-4 
                 border-black'>UI/UX</p>
             </div> 
@@ -25,14 +26,13 @@ const Uiux = () => {
 
                 <br />
 
-                <p className='xs:text-lg font-bold flex text-white'>
+                <p className='xs:text-2 font-bold flex text-white'>
                     Open Case Study <span className='flex align-middle mx-1 p-1'><FaLongArrowAltRight /></span> 
                 </p> 
                
-                <div className='
-                 flex justify-between items-center
-                 h-10  ml-[500px] md:w-full '>
-                    <img src={iphone}  alt=""/>
+                <div className='hidden md:flex justify-between items-center align-middle
+                 h-5  ml-[350px] mb-[150px] md:w-5/6'>
+                    <img src={iphone} alt=""/>
                 </div>
                      
             </div>                  
